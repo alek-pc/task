ca7bfee0842a10a8a4db66e0abdd6cac8ca3bf39
alek-pc <101040127+alek-pc@users.noreply.github.com>
2023-03-05 00:32:42 +0300

    Add files via upload

---
+++ data/jobs.py
@@ -0,0 +1,15 @@
+import sqlalchemy
+import datetime
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=True)
+    start_date = sqlalchemy.Column(sqlalchemy.Date, default=datetime.date.today())
+    end_date = sqlalchemy.Column(sqlalchemy.Date, default=datetime.date.today())
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
